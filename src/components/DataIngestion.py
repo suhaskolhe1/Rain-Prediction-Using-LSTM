712fc7063d701fe7c91e882050fab3bf017c8ca8
suhaskolhe11 <suhaskolhe1111@gmail.com>
2025-08-27 07:41:16 +0530

    DataIngestion: Initlial Class Done

--- src/components/DataIngestion.py
+++ src/components/DataIngestion.py
@@ -44,6 +44,18 @@ class DataIngestion:
 
             logging.info("Train-test split initiated")
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False)
+
+            logging.info("Train and test sets saved in processed folder")
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
+            )
+
         except Exception as e:
             pass
             raise CustomException(e,sys)
@@ -53,3 +65,4 @@ class DataIngestion:
 if __name__ == "__main__":
     obj = DataIngestion()
     obj.init_data_ingestion()
+    
